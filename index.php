386f16757cd47b1fcd7e59098146a94737a437c0
qteck <qteck@email.cz>
2015-02-04 09:58:44 +0000

    cannot redeclare class

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/vendor/tracy/tracy/src/tracy.php';
+require_once __DIR__ . '/model/default.php';
 
 use Tracy\Debugger;
 
@@ -7,57 +8,9 @@ session_start();
 
 Debugger::enable(Debugger::DEVELOPMENT);
 
-class PageMannager 
-{
-    public $page;
-    public $files;
-    
-    public function __construct() 
-    {
-        $this->page = isset($_GET['page'])? 
-                                (filter_input(INPUT_GET, 'page', FILTER_SANITIZE_FULL_SPECIAL_CHARS)):null;
 
-    }
-    
-    function checkDirectories($dir)
-    {
-                    if(is_dir($dir))
-                    {
-                        foreach (glob($dir . '*') as $value) {
-                            $e = array_reverse(explode('/', $value));
-                                
-                            $this->files[] = $e[0];
-                        }
-                    }  
-                    
-                    if($this->files && in_array($this->page, $this->files)) 
-                    {               
-                        include $dir . $this->page;
-                        
-                    } else 
-                    {
-                        include $dir . "default.php";
-                    }
-    }
-    
-            function modelIncluder()
-            {
-                $dir = __DIR__ . '/model/';
-                $this->checkDirectories($dir);
-            }
-            
-            function templateIncluder()
-            {
-                $dir = __DIR__ . '/templates/';
-                $this->checkDirectories($dir);
-                                   
-   
-
-            }
-}
-
-            $pages = new PageMannager();
-            $pages->modelIncluder();
+$pages = new Model\PageMannager;
+$pages->modelIncluder();
             
 ?>
 <!DOCTYPE html>
--- model/login.php
+++ model/login.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 
 require_once __DIR__ . "/../vendor/facebook/php-sdk-v4/autoload.php";
 
